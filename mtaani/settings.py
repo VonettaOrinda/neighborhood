ebe9499611c211240b21be8dc606f3d4bcdf48e9
VONETTA <thevonetteway@gmail.com>
2022-06-21 02:33:02 +0300

    added database to project settings

--- mtaani/settings.py
+++ mtaani/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = 'mtaani.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbourhood',
     }
 }
+
 
 
 # Password validation

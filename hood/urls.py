5bd5ff6b6444bc9c18b16f44a1bb13f0fd5e82c9
VONETTA <thevonetteway@gmail.com>
2022-06-21 15:40:28 +0300

    made changes to apps urls

--- hood/urls.py
+++ hood/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    # path('account/', include('django.contrib.auth.urls')),
-    re_path(r'', views.home, name='homePage'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'', views.home, name='home'),
     re_path(r'search/', views.search_businesses, name='search_businesses'),
     re_path(r'^join/(\d+)', views.join, name='join'),
     re_path(r'^exit/(\d+)', views.exit, name='exit'),
@@ -18,19 +18,18 @@ urlpatterns = [
     re_path(r'^users/', views.user_list, name='user_list'),
     re_path(r'^new/image$', views.new_image, name='new_image'),
     re_path(r'^new/business$', views.new_business, name='new_business'),
-    re_path(r'^new/project$', views.new_project, name='new_project'),
     re_path(r'^new/neighbourhood$', views.new_neighbourhood, name='new_neighbourhood'),
     re_path(r'^edit/profile$', views.edit_profile, name='edit_profile'),
     re_path(r'^profile/(?P<username>[0-9]+)$',
         views.individual_profile_page, name='individual_profile_page'),
     re_path(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
-    re_path(r'^$', views.review_list, name='review_list'),
+    # re_path(r'^$', views.review_list, name='review_list'),
     re_path(r'^review/(?P<review_id>[0-9]+)/$',
         views.review_detail, name='review_detail'),
-    re_path(r'^project$', views.project_list, name='project_list'),
+    
     
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
